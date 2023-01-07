bd835d4f932487005d5150473cfe797321cd742f
ext.andronic <alexandru.andronic@p4b.ro>
2023-01-07 12:11:36 +0200

    initial structure

---
+++ src/main/java/javanetcafe/appemployees/AppEmployeesApplication.java
@@ -0,0 +1,13 @@
+package javanetcafe.appemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javanetcafe/appemployees/AppEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package javanetcafe.appemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
